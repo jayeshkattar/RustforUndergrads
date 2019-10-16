39cdc28431213e71b97234e26e7fc429b31c52ff
ankan03 <1828049@kiit.ac.in>
2019-10-16 19:29:05 +0530

    3rd

--- 11 - Functions/functions/src/main.rs
+++ 11 - Functions/functions/src/main.rs
@@ -4,10 +4,11 @@ fn main() {
     main2(2);
 }
 
-fn main1(){
-    println!("I'm main1");
-}
+
 
 fn main2(a:i32){
     println!("What value am I getting = {}",a );
+}
+fn main3(){
+    println!("I'm main2");
 }
